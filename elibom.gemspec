3edfd65ac1a71149652bb43c414e34e425b3859f
German Escobar <german.escobarc@gmail.com>
2013-08-01 22:58:47 -0500

    Release version 0.4.0

--- elibom.gemspec
+++ elibom.gemspec
@@ -1,12 +1,13 @@
 Gem::Specification.new do |s|
   s.name        = 'elibom'
-  s.version     = '0.1.0'
-  s.date        = '2013-07-28'
+  s.version     = '0.4.0'
+  s.date        = '2013-08-01'
   s.summary     = "Elibom API client"
   s.description = "A client of the Elibom API"
   s.authors     = [ "German Escobar" ]
   s.email       = 'german.escobar@elibom.com'
   s.files       = ["lib/elibom.rb"]
-  s.homepage    =
-    'http://rubygems.org/gems/elibom'
+  s.homepage    = 'http://rubygems.org/gems/elibom'
+
+  s.add_dependency('json')
 end
--- lib/elibom.rb
+++ lib/elibom.rb
@@ -8,7 +8,7 @@ module Elibom
   class Client
 
     def initialize(options={})
-      @host = options[:host] || "http://www.elibom.com"
+      @host = options[:host] || "https://www.elibom.com"
       @user = options[:user]
       @api_password = options[:api_password]
 
@@ -19,11 +19,29 @@ module Elibom
     def send_message(args={})
       body = {}
 
-      required_args = [:destinations, :text]
+      required_args = [:to, :text]
       required_args.each do |arg|
         raise ArgumentError, "Missing key ':#{arg}'" if args[arg].nil?
         body[arg] = args[arg]
       end
+
+      post '/messages', body
+    end
+
+    def schedule_message(args={})
+      body = {}
+
+      required_args = [:to, :text]
+      required_args.each do |arg|
+        raise ArgumentError, "Missing key ':#{arg}'" if args[arg].nil?
+        body[arg] = args[arg]
+      end
+
+      raise ArgumentError, "Missing key ':schedule_date'" if args[:schedule_date].nil?
+      raise ArgumentError, "Invalid argument ':schedule_date'" unless args[:schedule_date].respond_to?('strftime')
+
+      body['scheduleDate'] =  args[:schedule_date].strftime('%Y-%m-%d %H:%M')
+      puts body
 
       post '/messages', body
     end
@@ -33,6 +51,28 @@ module Elibom
       get "/messages/#{delivery_id}"
     end
     alias :list_messages :messages
+
+    def scheduled
+      get "/schedules/scheduled"
+    end
+    alias :list_scheduled_messages :scheduled
+    alias :schedules :scheduled
+    alias :list_schedules :scheduled
+
+    def schedule(schedule_id)
+      raise ArgumentError, "'schedule_id' cannot be nil" if schedule_id.nil?
+      get "/schedules/#{schedule_id}"
+    end
+
+    def cancel_schedule(schedule_id)
+      raise ArgumentError, "'schedule_id' cannot be nil" if schedule_id.nil?
+      delete "/schedules/#{schedule_id}"
+    end
+    alias :unschedule :cancel_schedule
+
+    def users
+      get "/users"
+    end
 
     def user(user_id)
       raise ArgumentError, "'user_id' cannot be nil" if user_id.nil?
@@ -47,25 +87,19 @@ module Elibom
 
       def get(resource)
         uri = URI.parse("#{@host}#{resource}")
-        http = Net::HTTP.new(uri.host, uri.port)
+        http = get_http(uri)
 
         request = Net::HTTP::Get.new(uri.request_uri)
         request.basic_auth @user, @api_password
         request['Accept'] = 'application/json'
 
         response = http.request(request)
-
-        case response
-          when Net::HTTPSuccess
-            JSON.parse(response.body)
-          else
-            response.error!
-        end
+        parse_json_response(response)
       end
 
       def post(resource, body)
         uri = URI.parse("#{@host}#{resource}")
-        http = Net::HTTP.new(uri.host, uri.port)
+        http = get_http(uri)
 
         request = Net::HTTP::Post.new(uri.path)
         request.basic_auth @user, @api_password
@@ -74,7 +108,29 @@ module Elibom
         request.body = body.to_json
 
         response = http.request(request)
+        parse_json_response(response)
+      end
 
+      def delete(resource)
+        uri = URI.parse("#{@host}#{resource}")
+        http = get_http(uri)
+
+        request = Net::HTTP::Delete.new(uri.path)
+        request.basic_auth @user, @api_password
+        
+        http.request(request)
+      end
+
+      def get_http(uri)
+        http = Net::HTTP.new(uri.host, uri.port)
+        if uri.scheme == 'https'
+          http.use_ssl = true
+        end
+
+        return http
+      end
+
+      def parse_json_response(response)
         case response
           when Net::HTTPSuccess
             JSON.parse(response.body)
@@ -82,6 +138,5 @@ module Elibom
             response.error!
         end
       end
-
   end
 end
